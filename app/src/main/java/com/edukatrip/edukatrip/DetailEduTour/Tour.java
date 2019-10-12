d4d26cb1f233567dcea4aff3edcff4cbba16e502
dimaskris98 <dimasc1945@gmail.com>
2019-10-12 23:21:57 +0700

    Add List Menu Educational Tour

---
+++ app/src/main/java/com/edukatrip/edukatrip/DetailEduTour/Tour.java
@@ -0,0 +1,63 @@
+package com.edukatrip.edukatrip.DetailEduTour;
+
+public class Tour {
+    private int image,bintang;
+    String info,judul,harga,review;
+
+    public int getBintang() {
+        return bintang;
+    }
+
+    public void setBintang(int bintang) {
+        this.bintang = bintang;
+    }
+
+    public Tour(int image, String info, String judul, String harga,int bintang, String review) {
+        this.image = image;
+        this.info = info;
+        this.judul = judul;
+        this.harga = harga;
+        this.bintang = bintang;
+        this.review = review;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+}
---
+++ app/src/main/java/com/edukatrip/edukatrip/DetailEduTour/customGridView.java
@@ -0,0 +1,31 @@
+package com.edukatrip.edukatrip.DetailEduTour;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+public class customGridView extends GridView {
+    public customGridView(Context context) {
+        super(context);
+    }
+
+    public customGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public customGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightSpec = heightMeasureSpec;
+        if (getLayoutParams().height == LayoutParams.WRAP_CONTENT) {
+            // The great Android "hackatlon", the love, the magic.
+            // The two leftmost bits in the height measure spec have
+            // a special meaning, hence we can't use them to describe height.
+            heightSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        }
+        super.onMeasure(widthMeasureSpec, heightSpec);
+    }
+}
