d333c561c58a62f7eb2b5f911615df308682389c
Regawa <regawa04@gmail.com>
2019-10-13 19:59:08 +0700

    (+) MainActivity, Bottom Navigation (+) Home Fragment Layout

--- app/src/main/java/com/edukatrip/edukatrip/MainActivity.java
+++ app/src/main/java/com/edukatrip/edukatrip/MainActivity.java
@@ -1,14 +1,55 @@
 package com.edukatrip.edukatrip;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    HomeFragment homeFragment = new HomeFragment();
+                    openFragment(homeFragment);
+                    return true;
+                case R.id.nav_transaction:
+                    return true;
+                case R.id.nav_trips:
+                    return true;
+                case R.id.nav_inbox:
+                    return true;
+                case R.id.nav_setting:
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        HomeFragment homeFragment = new HomeFragment();
+        openFragment(homeFragment);
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
