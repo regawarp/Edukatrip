097d9032007abd15a80ddc6c4c701e5875bfc3e0
Regawa <regawa04@gmail.com>
2019-11-03 15:53:34 +0700

    Layout "Payment Report"

---
+++ app/src/main/java/com/edukatrip/edukatrip/PaymentReportActivity.java
@@ -0,0 +1,14 @@
+package com.edukatrip.edukatrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_report);
+    }
+}
